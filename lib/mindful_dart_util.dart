639efd6c8edfb48d2673c13f3e64f904e0b5e1b0
Michael Bushe <michael@bushe.com>
2023-01-20 03:25:04 +0800

    TO fix lack of api availablity, compared to a dart gen'ed lib and reflected that in the imports.

--- lib/mindful_dart_util.dart
+++ lib/mindful_dart_util.dart
@@ -1,5 +1,4 @@
 /// Utilities that only depend on Dart.
 library mindful_dart_util;
 
-export 'src/file_util.dart';
-export 'src/mindful_dart_util.dart';
+export 'src/mindful_dart_util_base.dart';
--- lib/src/mindful_dart_util_base.dart
+++ lib/src/mindful_dart_util_base.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+/// Utilities that only depend on Dart.
+
 /// Returns the user's home directory or null if none specified.
 Directory? userHome() {
   final homePath =
@@ -8,5 +10,12 @@ Directory? userHome() {
     return null;
   } else {
     return Directory(homePath);
+  }
+}
+
+/// TODO: Remove me.  Testing lib exposure.
+class MindfulDartUtil {
+  static Directory? getHome() {
+    return userHome();
   }
 }
