b33f0a85c2c40f07df5a74875f3ec42076eb3649
Michael Bushe <michael@bushe.com>
2023-01-20 02:16:15 +0800

    still not showing up with public apis.

--- lib/src/mindful_dart_util.dart
+++ lib/src/mindful_dart_util.dart
@@ -4,8 +4,7 @@ import 'package:mindful_dart_util/mindful_dart_util.dart';
 
 /// Utilities that only depend on Dart.
 class MindfulDartUtil {
-  const MindfulDartUtil();
-  Directory? getHome() {
+  static Directory? getHome() {
     return userHome();
   }
 }
